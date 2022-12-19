863b84d8f6f26f282b776596a8cd30f3a902c208
jnutterdev <hello@jnutterdev.com>
2022-12-19 16:14:56 -0500

    Image cards can now display an image if uploaded to django server

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -33,7 +33,7 @@ class CharacterCard extends Component {
               <div className="card-image">
                 <figure className="image is-4by3">
                   <img
-                    src="https://bulma.io/images/placeholders/1280x960.png"
+                    src={character.character_image || 'https://bulma.io/images/placeholders/96x96.png'}
                     width="300"
                     alt="Placeholder image"
                   />
